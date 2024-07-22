5ccbb165228936763dfb2398090d8c1a556541c7
pw598 <106397483+pw598@users.noreply.github.com>
2024-07-22 10:56:58 -0600

    Add files via upload

---
+++ excel/VBA/js/subj_list.js
@@ -0,0 +1,41 @@
+document.getElementById("subj1").innerHTML = "Config &amp; VB Editor";
+document.getElementById("subj1").href = "subjects/config_vbeditor/config_vbeditor.html";
+
+document.getElementById("subj2").innerHTML = "Code Fundamentals";
+document.getElementById("subj2").href = "subjects/recorder_fundamentals/recorder_fundamentals.html";
+
+document.getElementById("subj3").innerHTML = "Workbooks, Sheets, &amp; OOP";
+document.getElementById("subj3").href = "subjects/wbk_wsht_oop/wbk_wsht_oop.html";
+
+document.getElementById("subj4").innerHTML = "Range Manipulation";
+document.getElementById("subj4").href = "subjects/ranges/ranges.html";
+
+document.getElementById("subj5").innerHTML = "Variables";
+document.getElementById("subj5").href = "subjects/variables/variables.html";
+
+document.getElementById("subj6").innerHTML = "Conditional Statements";
+document.getElementById("subj6").href = "subjects/conditional_statements/conditional_statements.html";
+
+document.getElementById("subj7").innerHTML = "Loops";
+document.getElementById("subj7").href = "subjects/loops/loops.html";
+
+document.getElementById("subj8").innerHTML = "Arrays";
+document.getElementById("subj8").href = "subjects/arrays/arrays.html";
+
+document.getElementById("subj9").innerHTML = "Built-In Functions";
+document.getElementById("subj9").href = "subjects/built_in_functions/built_in_functions.html";
+
+document.getElementById("subj10").innerHTML = "Events";
+document.getElementById("subj10").href = "subjects/events/events.html";
+
+document.getElementById("subj11").innerHTML = "Files and Folders";
+document.getElementById("subj11").href = "subjects/files_folders/files_folders.html";
+
+
+/*
+
+error handling and debugging
+interacting with other applications
+charts and pivot tables
+
+*/
